9ef49dc34e173f34d9448be434725250f627fb9b
ljupce99 <ljupcejovanov99@gmail.com>
2024-12-25 14:49:12 +0100

    Add files via upload

--- src/main/java/mk/ukim/finki/lab/web/Config/SecurityConfig.java
+++ src/main/java/mk/ukim/finki/lab/web/Config/SecurityConfig.java
@@ -2,10 +2,8 @@ package mk.ukim.finki.lab.web.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.jaas.memory.InMemoryConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -13,11 +11,10 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
-    private final PasswordEncoder passwordEncoder;
 
-    public SecurityConfig(PasswordEncoder passwordEncoder) {
-        this.passwordEncoder = passwordEncoder;
-    }
+
+
+
 //    @Bean
 //    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 //        http .csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests().anyRequest().permitAll(); // Дозволи пристап насекаде
@@ -35,11 +32,11 @@ public class SecurityConfig {
                         .permitAll()
                         .requestMatchers("/addcomment")
                         .hasRole("USER")
-                        .requestMatchers("/songs","/songs/**","/addcomment")
+                        .requestMatchers("/**")
                         .hasRole("ADMIN")
+
                 )
                 .formLogin((form) -> form
-                        .loginPage("/login")
                         .permitAll()
                         .failureUrl("/login?error=BadCredentials")
                         .defaultSuccessUrl("/listSongs", true)
@@ -54,7 +51,7 @@ public class SecurityConfig {
 
         return http.build();
     }
-//inMemoryAuthentication
+
     @Bean
     public InMemoryUserDetailsManager inMemoryAuthentication(PasswordEncoder passwordEncoder) {
         return new InMemoryUserDetailsManager(
