666e71bb02c32a001f2124bc20566c52d4d0e067
ljupce99 <ljupcejovanov99@gmail.com>
2024-12-25 22:54:56 +0100

    Add files via upload

--- src/main/java/mk/ukim/finki/lab/web/Config/SecurityConfig.java
+++ src/main/java/mk/ukim/finki/lab/web/Config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -17,33 +18,39 @@ public class SecurityConfig {
 
 //    @Bean
 //    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http .csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests().anyRequest().permitAll(); // Дозволи пристап насекаде
+//        http .csrf(AbstractHttpConfigurer::disable)
+//        .authorizeHttpRequests()
+//        .anyRequest()
+//        .permitAll(); // Дозволи пристап насекаде
 //        return http.build();
 //    }
 
 
-        @Bean
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .headers((headers)->headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
                 .authorizeHttpRequests(authorizeRequests ->authorizeRequests
-                        .requestMatchers("/login","/listSongs","/Details","/artist")
+                        .requestMatchers("/login","/logout")
                         .permitAll()
-                        .requestMatchers("/addcomment","/songs/Details/*","/songs")
-                        .hasRole("USER")
-                        .requestMatchers("/**")
+                        .requestMatchers("/listSongs","/Details","/artist","/addcomment","/songs","/songs/Details/*")
+                        .hasAnyRole("USER","ADMIN")
+                        .requestMatchers("/songs/**","/**")
                         .hasRole("ADMIN")
+                        .anyRequest()
+                        .authenticated()
 
                 )
                 .formLogin((form) -> form
                         .permitAll()
-                        .failureUrl("/login?error=BadCredentials")
+                        .failureUrl("/login?error=ImasGreska")
                         .defaultSuccessUrl("/listSongs", true)
                 )
                 .logout((logout) -> logout
                         .logoutUrl("/logout")
-                        .logoutSuccessUrl("/listSongs")
+                        .logoutSuccessUrl("/login")
                         .permitAll()
 
                 );
